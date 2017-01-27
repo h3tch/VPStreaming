645927509cd56e893b39bba396308f8c708ea7a8
h3tch <michael.hecher@gmx.net>
2017-01-27 14:42:12 +0100

    Initial commit

---
+++ src/FormVPStreaming.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace VPStreaming
+{
+    partial class FormVPStreaming
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            this.TextIpAddress = new System.Windows.Forms.TextBox();
+            this.TextPassword = new System.Windows.Forms.TextBox();
+            this.PictureLogo = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.ButtonStart, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.TextIpAddress, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.TextPassword, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.PictureLogo, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(578, 294);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP-Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(217, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ButtonStart.Location = new System.Drawing.Point(431, 256);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(144, 35);
+            this.ButtonStart.TabIndex = 2;
+            this.ButtonStart.Text = "Start";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // TextIpAddress
+            // 
+            this.TextIpAddress.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TextIpAddress.Location = new System.Drawing.Point(3, 256);
+            this.TextIpAddress.Name = "TextIpAddress";
+            this.TextIpAddress.Size = new System.Drawing.Size(208, 26);
+            this.TextIpAddress.TabIndex = 3;
+            this.TextIpAddress.WordWrap = false;
+            // 
+            // TextPassword
+            // 
+            this.TextPassword.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TextPassword.Location = new System.Drawing.Point(217, 256);
+            this.TextPassword.Name = "TextPassword";
+            this.TextPassword.Size = new System.Drawing.Size(208, 26);
+            this.TextPassword.TabIndex = 4;
+            this.TextPassword.WordWrap = false;
+            // 
+            // PictureLogo
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.PictureLogo, 3);
+            this.PictureLogo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PictureLogo.Location = new System.Drawing.Point(3, 3);
+            this.PictureLogo.Name = "PictureLogo";
+            this.PictureLogo.Size = new System.Drawing.Size(572, 227);
+            this.PictureLogo.TabIndex = 5;
+            this.PictureLogo.TabStop = false;
+            // 
+            // FormVPStreaming
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 294);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 350);
+            this.MinimumSize = new System.Drawing.Size(600, 350);
+            this.Name = "FormVPStreaming";
+            this.Text = "VP Streaming";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ButtonStart;
+        private System.Windows.Forms.TextBox TextIpAddress;
+        private System.Windows.Forms.TextBox TextPassword;
+        private System.Windows.Forms.PictureBox PictureLogo;
+    }
+}
+
---
+++ src/FormVPStreaming.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace VPStreaming
+{
+    public partial class FormVPStreaming : Form
+    {
+        public FormVPStreaming()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            // get environment variables that contain the gstreamer root directory
+            var gstDirectory = Environment.GetEnvironmentVariable("GSTREAMER_1_0_ROOT_X86");
+            if (gstDirectory == null)
+                gstDirectory = Environment.GetEnvironmentVariable("GSTREAMER_1_0_ROOT_X86_64");
+
+            // set default values for gstreamer
+            var gst_exe = "gst-launch-1.0";
+            var gst_launch = gstDirectory != null ? $"{gstDirectory}bin/" : "./";
+            var gst_pipeline = "videotestsrc ! autovideosink";
+            
+            // load settings xml file
+            var xmlDocument = new XmlDocument();
+            try
+            {
+                xmlDocument.Load("settings.xml");
+
+                var exe = xmlDocument["Settings"]?["gstreamer"]?["executable"]?.InnerText;
+                var launch = xmlDocument["Settings"]?["gstreamer"]?["directory"]?.InnerText;
+                var pipeline = xmlDocument["Settings"]?["gstreamer"]?["pipeline"]?.InnerText;
+
+                if (exe != null && exe.Length > 0)
+                    gst_exe = exe;
+                if (launch != null && launch.Length > 0)
+                    gst_launch = launch;
+                if (pipeline != null && pipeline.Length > 0)
+                    gst_pipeline = pipeline;
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message, "File not found");
+            }
+
+            // set process info
+            var startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            startInfo.FileName = $"{gst_launch}{gst_exe}";
+            startInfo.Arguments = gst_pipeline;
+
+            // create process
+            var process = new Process();
+            process.StartInfo = startInfo;
+
+            try
+            {
+                // start process
+                process.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+    }
+}
