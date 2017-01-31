fa5378bc2d5ff6ec6124f4050c29c58b2c3ef06a
Michael Hecher <m.hecher@viewpointsystem.com>
2017-01-31 17:51:20 +0100

    Bug fix.

--- src/FormVPStreaming.cs
+++ src/FormVPStreaming.cs
@@ -10,10 +10,16 @@ namespace VPStreaming
 {
     public partial class FormVPStreaming : Form
     {
+        private XmlDocument XmlSettings = new XmlDocument();
+
         public FormVPStreaming()
         {
+            // load settings xml file
+            XmlSettings.Load("settings.xml");
+
             InitializeComponent();
             LoadLogo("logo.png");
+            LoadDefaults();
         }
 
         /// <summary>
@@ -41,6 +47,19 @@ namespace VPStreaming
         }
 
         /// <summary>
+        /// Load the defaults.
+        /// </summary>
+        private void LoadDefaults()
+        {
+            if (XmlSettings != null)
+            {
+                var IP = XmlSettings["Settings"]?["defaultIP"]?.InnerText;
+                if (IP != null && IP.Length > 0)
+                    TextIpAddress.Text = IP;
+            }
+        }
+
+        /// <summary>
         /// Click event of the start button.
         /// </summary>
         /// <param name="sender">Sender object</param>
@@ -57,31 +76,32 @@ namespace VPStreaming
             var gst_launch = gstDirectory != null ? $"{gstDirectory}bin/" : "./";
             var gst_pipeline = "videotestsrc ! autovideosink";
 
-            try
+            if (XmlSettings != null)
             {
-                // load settings xml file
-                var xmlDocument = new XmlDocument();
-                xmlDocument.Load("settings.xml");
-
-                // get settings specified in the xml
-                var exe = xmlDocument["Settings"]?["gstreamer"]?["executable"]?.InnerText;
-                var launch = xmlDocument["Settings"]?["gstreamer"]?["directory"]?.InnerText;
-                var pipeline = xmlDocument["Settings"]?["gstreamer"]?["pipeline"]?.InnerText;
-
-                // update values if necessary
-                if (exe != null && exe.Length > 0)
-                    gst_exe = exe;
-                if (launch != null && launch.Length > 0)
-                    gst_launch = launch;
-                if (pipeline != null && pipeline.Length > 0)
+                try
                 {
-                    pipeline = Regex.Replace(pipeline, "{IP}", TextIpAddress.Text);
-                    gst_pipeline = Regex.Replace(pipeline, "{password}", TextPassword.Text);
+                    // get settings specified in the xml
+                    var exe = XmlSettings["Settings"]?["gstreamer"]?["executable"]?.InnerText;
+                    var launch = XmlSettings["Settings"]?["gstreamer"]?["directory"]?.InnerText;
+                    var pipeline = TextPassword.Text.Length > 0
+                        ? XmlSettings["Settings"]?["gstreamer"]?["pipelinePW"]?.InnerText
+                        : XmlSettings["Settings"]?["gstreamer"]?["pipeline"]?.InnerText;
+
+                    // update values if necessary
+                    if (exe != null && exe.Length > 0)
+                        gst_exe = exe;
+                    if (launch != null && launch.Length > 0)
+                        gst_launch = launch;
+                    if (pipeline != null && pipeline.Length > 0)
+                    {
+                        pipeline = Regex.Replace(pipeline, "{IP}", TextIpAddress.Text);
+                        gst_pipeline = Regex.Replace(pipeline, "{password}", TextPassword.Text);
+                    }
                 }
-            }
-            catch (FileNotFoundException ex)
-            {
-                MessageBox.Show(ex.Message, "File not found");
+                catch (FileNotFoundException ex)
+                {
+                    MessageBox.Show(ex.Message, "File not found");
+                }
             }
 
             // set process info
@@ -89,6 +109,8 @@ namespace VPStreaming
             startInfo.WindowStyle = ProcessWindowStyle.Normal;
             startInfo.FileName = $"{gst_launch}{gst_exe}";
             startInfo.Arguments = gst_pipeline;
+            //startInfo.WorkingDirectory = gst_launch;
+            //startInfo.UseShellExecute = true;
 
             // create process
             var process = new Process();
